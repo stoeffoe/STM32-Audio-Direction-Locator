a32485f5d75820073922eb0389ecbd4a85668317
Stefan Beenen <Stefan.Beenen@hotmail.com>
2021-06-26 12:11:20 +0200

    added method airspeed calculation

---
+++ SoundLocalization.cpp
@@ -0,0 +1,11 @@
+
+//
+//
+//float soundSpeedFromTempKelvin(float airTempKelvin){
+//    return 331.3 * sqrt(1+ airTempKelvin);
+//}
+//
+//
+//float soundSpeedFromTempCelsius(float airTempCelsius){
+//    return  soundSpeedFromTempKelvin(273.15 + airTempCelsius);
+//}
