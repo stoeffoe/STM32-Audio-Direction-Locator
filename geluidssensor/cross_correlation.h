aa0ca83ba97b154c8cccd7cb2e9767e174d7506d
Stefan Beenen <Stefan.Beenen@hotmail.com>
2021-06-28 09:45:48 +0200

    removed copies

--- geluidssensor/cross_correlation.h
+++
@@ -1,9 +0,0 @@
-#ifndef CROSS-CORRELATION_H
-#define CROSS-CORRELATION_H
-
-#include <stdint.h>
-
-int16_t calculateDelay(int16_t s1[], int16_t s2[], uint16_t n, uint16_t maxDelay);
-void filterData(int16_t data[], uint16_t n);
-
-#endif /*CROSS-CORRELATION_H*/
--- geluidssensor/soundSpeed.h
+++
@@ -1,8 +0,0 @@
-#ifndef SOUNDSPEED_H
-#define SOUNDSPEED_H
-
-float soundSpeedFromTempKelvin(float airTempKelvin);
-
-float soundSpeedFromTempCelsius(float airTempCelsius);
-
-#endif /*SOUNDSPEED_H*/
